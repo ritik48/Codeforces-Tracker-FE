c6fc569966e14275e8fa8a74994586b5eb30325e
Ritik <raj769417@gmail.com>
2025-06-17 11:19:57 +0530

    .\src\components\ui\badge.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,7 +5,6 @@ export const NavBar = () => {
     <div className="border-b">
       <div className="flex items-center justify-between max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-2">
         <div className="font-bold">CF Management</div>
-
         <div>
           <Button size={"sm"}>Login</Button>
         </div>
