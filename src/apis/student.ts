113b9a6023a3dbb1887fb4b70a7d8be0e36324f9
Ritik <raj769417@gmail.com>
2025-06-17 23:56:34 +0530

    feat: api to create and edit student

--- src/apis/student.ts
+++ src/apis/student.ts
@@ -1,3 +1,5 @@
+import type { StudentFormType } from "@/pages/Students/components/CreateAndEditStudentForm";
+
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
 
 export const fetchAllStudents = async (page = 1, limit = 10) => {
@@ -27,6 +29,102 @@ export const fetchAllStudents = async (page = 1, limit = 10) => {
     return {
       success: false,
       message: "Failed to fetch students due to an error",
+    };
+  }
+};
+
+export const deleteStudent = async (studentId: string) => {
+  try {
+    const response = await fetch(`${BACKEND_URL}/student/${studentId}`, {
+      method: "DELETE",
+      credentials: "include",
+    });
+
+    if (!response.ok) {
+      const data = await response.json();
+      return {
+        success: false,
+        message: data.message || "Failed to delete student",
+      };
+    }
+
+    return { success: true, message: "Student deleted successfully" };
+  } catch (error) {
+    console.error("Failed to delete student:", error);
+    return {
+      success: false,
+      message: "Failed to delete student due to an error",
+    };
+  }
+};
+
+export const createStudentApi = async (studentData: StudentFormType) => {
+  try {
+    const response = await fetch(`${BACKEND_URL}/student`, {
+      method: "POST",
+      credentials: "include",
+      body: JSON.stringify(studentData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    if (!response.ok) {
+      return {
+        success: false,
+        message: data.message || "Failed to create student",
+      };
+    }
+
+    return {
+      success: true,
+      data: data.data,
+      message: "Student created successfully",
+    };
+  } catch (error) {
+    console.error("Failed to create student:", error);
+    return {
+      success: false,
+      message: "Failed to create student due to an error",
+    };
+  }
+};
+
+export const editStudentApi = async (
+  studentId: string,
+  studentData: StudentFormType
+) => {
+  try {
+    await new Promise((resolve) => setTimeout(resolve, 5000)); // Simulate network delay
+
+    const response = await fetch(`${BACKEND_URL}/student/${studentId}`, {
+      method: "PATCH",
+      credentials: "include",
+      body: JSON.stringify(studentData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    if (!response.ok) {
+      return {
+        success: false,
+        message: data.message || "Failed to edit student",
+      };
+    }
+
+    return {
+      success: true,
+      data: data.data,
+      message: "Student edited successfully",
+    };
+  } catch (error) {
+    console.error("Failed to edit student:", error);
+    return {
+      success: false,
+      message: "Failed to edit student due to an error",
     };
   }
 };
