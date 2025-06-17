614ed9880744472a4cbb4e8da2b1dd068a81600d
Ritik <raj769417@gmail.com>
2025-06-17 21:22:17 +0530

    feat: used student table

--- src/pages/Students/Student.tsx
+++ src/pages/Students/Student.tsx
@@ -1,3 +1,11 @@
+import { StudentTable } from "./components/StudentTable";
+
 export function Student() {
-  return <div>Student</div>;
+  return (
+    <div>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
+        <StudentTable />
+      </div>
+    </div>
+  );
 }
