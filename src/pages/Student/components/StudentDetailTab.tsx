f143ea133c89b11da1cd9f760e085c39fe8eee34
Ritik <raj769417@gmail.com>
2025-06-18 13:50:00 +0530

    feat: date filter state and stylde changes

--- src/pages/Student/components/StudentDetailTab.tsx
+++ src/pages/Student/components/StudentDetailTab.tsx
@@ -1,23 +1,13 @@
-import { AppWindowIcon, CodeIcon } from "lucide-react";
-
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useSearchParams } from "react-router-dom";
 import { ContestHistory } from "./ContestHistory";
+import { DateFilter } from "./DateFilter";
+import { useState } from "react";
 
 export function StudentDetailsTab() {
   const [searchParams, setSearchParams] = useSearchParams();
   const currentTab = searchParams.get("tab") || "contest-history";
+  const [days, setDays] = useState(30);
 
   const handleTabChange = (tab: string) => {
     setSearchParams((prev) => {
@@ -28,18 +18,23 @@ export function StudentDetailsTab() {
   };
 
   return (
-    <div className="flex w-full max-w-sm flex-col gap-6">
+    <div className="w-full gap-2">
       <Tabs value={currentTab} onValueChange={handleTabChange}>
-        <TabsList>
-          <TabsTrigger value="contest-history">Contest History</TabsTrigger>
-          <TabsTrigger value="submission-data">Submission Data</TabsTrigger>
-        </TabsList>
-        <TabsContent value="contest-history">
-          <ContestHistory />
-        </TabsContent>
-        <TabsContent value="submission-data">
-          <div>Submission Data</div>
-        </TabsContent>
+        <div className="flex w-full gap-3">
+          <TabsList>
+            <TabsTrigger value="contest-history">Contest History</TabsTrigger>
+            <TabsTrigger value="submission-data">Submission Data</TabsTrigger>
+          </TabsList>
+          <DateFilter days={days} setDays={setDays} />
+        </div>
+        <div className="">
+          <TabsContent value="contest-history" className="">
+            <ContestHistory days={days} />
+          </TabsContent>
+          <TabsContent value="submission-data">
+            <div>Submission Data</div>
+          </TabsContent>
+        </div>
       </Tabs>
     </div>
   );
