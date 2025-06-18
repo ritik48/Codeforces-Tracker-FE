7fff75d09acfb99607b74ca4f1e4356ec5b42b0d
Ritik <raj769417@gmail.com>
2025-06-19 03:08:03 +0530

    refactor: style changes and minor fix

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,8 @@ import { Button } from "./ui/button";
 import { Link, NavLink } from "react-router-dom";
 
 export const NavBar = () => {
-  const { username, logout } = useAuth();
+  const { username, logout, loading } = useAuth();
+
   return (
     <div className="border-b sticky top-0 z-10 bg-secondary">
       <div className="flex items-center justify-between max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-2">
@@ -15,33 +16,38 @@ export const NavBar = () => {
           <Link to={"/"} className="font-bold sm:hidden block">
             CF
           </Link>
-          <NavLink
-            to="/students"
-            className="text-sm text-gray-500 hover:text-gray-700"
-          >
-            Students
-          </NavLink>
-          <NavLink
-            to="/setting"
-            className="text-sm text-gray-500 hover:text-gray-700"
-          >
-            Setting
-          </NavLink>
+          {username && !loading && (
+            <div className="flex gap-6 sm:gap-10">
+              <NavLink
+                to="/students"
+                className="text-sm text-gray-500 hover:text-gray-700"
+              >
+                Students
+              </NavLink>
+              <NavLink
+                to="/setting"
+                className="text-sm text-gray-500 hover:text-gray-700"
+              >
+                Setting
+              </NavLink>
+            </div>
+          )}
         </div>
         <div className="flex items-center gap-3">
-          {username && (
+          {username && !loading && (
             <span className="sm:block hidden">Welcome👋, {username}</span>
           )}
           <ModeToggle />
-          {username ? (
-            <Button size={"sm"} variant={"outline"} onClick={logout}>
-              Logout
-            </Button>
-          ) : (
-            <Button size={"sm"}>
-              <Link to="/login">Login</Link>
-            </Button>
-          )}
+          {!loading &&
+            (username ? (
+              <Button size="sm" variant="outline" onClick={logout}>
+                Logout
+              </Button>
+            ) : (
+              <Button size="sm" asChild>
+                <Link to="/login">Login</Link>
+              </Button>
+            ))}
         </div>
       </div>
     </div>
