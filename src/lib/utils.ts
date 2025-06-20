0c4bd4a7c056ff60a62bb8dcbe7c018af0b81db4
Ritik <raj769417@gmail.com>
2025-06-20 15:34:57 +0530

    feat: export cron day mapper

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,26 +9,24 @@ type ParsedCron = {
   time: string; //  "22:30"
 };
 
-export function parseCronExpression(cron: string): ParsedCron {
-  const parts = cron.trim().split(" ");
+// Reverse map for day
+export const cronToDay: Record<string, string> = {
+  "*": "daily",
+  "0": "sun",
+  "1": "mon",
+  "2": "tue",
+  "3": "wed",
+  "4": "thu",
+  "5": "fri",
+  "6": "sat",
+};
 
-  const [minuteStr, hourStr, , , dayOfWeek] = parts;
-  const minute = parseInt(minuteStr);
-  const hour = parseInt(hourStr);
-
-  // Reverse map for day
-  const dayMap: Record<string, string> = {
-    "*": "daily",
-    "0": "sun",
-    "1": "mon",
-    "2": "tue",
-    "3": "wed",
-    "4": "thu",
-    "5": "fri",
-    "6": "sat",
-  };
-
-  const day = dayMap[dayOfWeek] ?? "daily";
+export function parseCronExpression(
+  minute: number,
+  hour: number,
+  dayOfWeek: string
+): ParsedCron {
+  const day = cronToDay[dayOfWeek] ?? "daily";
 
   // converts 1 to 01, 2 to 02, etc.
   const formattedTime = `${hour.toString().padStart(2, "0")}:${minute
@@ -42,7 +40,7 @@ export function parseCronExpression(cron: string): ParsedCron {
 }
 
 export const generateCron = (time: string, day: string) => {
-  const daysMap: Record<string, string> = {
+  const dayToCron: Record<string, string> = {
     daily: "*",
     sun: "0",
     mon: "1",
@@ -54,6 +52,37 @@ export const generateCron = (time: string, day: string) => {
   };
 
   const [hour, minute] = time.split(":").map((x) => parseInt(x));
-  const dayOfWeek = daysMap[day];
+  const dayOfWeek = dayToCron[day];
   return `${minute} ${hour} * * ${dayOfWeek}`;
+};
+
+export const convertToUtc = (time: string) => {
+  // Parse hour and minute
+  const [hour, minute] = time.split(":").map(Number);
+
+  // Create a dummy local Date (any day, just for time conversion)
+  const local = new Date();
+  local.setHours(hour);
+
+  local.setMinutes(minute);
+
+  // Convert to UTC hour and minute
+  const utcHours = local.getUTCHours();
+  const utcMinutes = local.getUTCMinutes();
+
+  // Format back as HH:MM
+  const pad = (n: number) => n.toString().padStart(2, "0");
+
+  return `${pad(utcHours)}:${pad(utcMinutes)}`;
+};
+
+export const getLocalTime = (minute: number, hour: number) => {
+  const local = new Date();
+  local.setUTCMinutes(minute);
+  local.setUTCHours(hour);
+
+  const localMinute = local.getMinutes();
+  const localHour = local.getHours();
+
+  return [localMinute, localHour];
 };
